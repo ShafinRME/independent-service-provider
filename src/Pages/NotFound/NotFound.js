36eb2b291c181d92804017818d25e87297b15d7f
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-18 12:44:43 +0600

    Not found page is done

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NotFound.css'
+
+const NotFound = () => {
+    return (
+        <div className='not-found'>
+
+            <h2>Hei dude .This is not for you.</h2>
+            <h4>But there is a funny pic for you.</h4>
+
+
+        </div>
+    );
+};
+
+export default NotFound;
