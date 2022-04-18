98711d681489826d88c06bf69a6806ad51c7845d
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-18 17:10:16 +0600

    Website name part done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,10 +3,22 @@ import Banner from '../Banner/Banner';
 import Faq from '../Faq/Faq';
 import Services from '../Services/Services';
 import './Home.css'
+import pic from '../../../images/Website-head.png'
 
 const Home = () => {
     return (
-        <div>
+        <div >
+            <h1 className='website-name'>Einstein School</h1>
+            <div className='web-head'>
+                <div className='web-head-title'>
+                    <h3>Learn Something</h3>
+                    <h4>With</h4>
+                    <h3>Infinity Fun</h3>
+                </div>
+                <div>
+                    <img src={pic} alt="" />
+                </div>
+            </div>
             <Banner></Banner>
             <Services></Services>
             <Faq></Faq>
